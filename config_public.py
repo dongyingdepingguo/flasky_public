3f568cdc58635bfb1aa2c4a07607f90a6e1fd5b9
dongyingdepingguo <2867432325@qq.com>
2019-09-09 13:02:59 +0800

    修改部分配置文件

--- config_public.py
+++ config_public.py
@@ -1,21 +1,18 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Time    : 2019/8/22 13:22
-# @Author  : zhangshaobin
-# @Site    :
-# @File    : config.py
-# @Software: PyCharm
+# !/usr/bin/env python
+
+# _*_ coding: utf-8 _*_
+
 
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    SECRET_KEY = 'hard to guess string'
+    SECRET_KEY = '***********'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     FLASKY_MAIL_SUBJECT_PREFIX = 'Flasky'
-    FLASKY_MAIL_SENDER = '2867432325@qq.com'
+    FLASKY_MAIL_SENDER = '*********@qq.com'
     FLASKY_POSTS_PER_PAGE = 3
 
 
@@ -23,17 +20,17 @@ class DevelopmentConfig(Config):
     DEBUG = True
     FLASKY_ADMIN = Config.FLASKY_MAIL_SENDER
     MAIL_SERVER = 'smtp.qq.com'
-    MAIL_PORT = 587
+    MAIL_PORT = 123
     MAIL_USE_TLS = True
-    MAIL_USERNAME = '2867432325@qq.com'
-    MAIL_RECEIVER = '3330602376@qq.com'
-    MAIL_PASSWORD = 'yfexvjdytugzdfbf'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:31415926z@127.0.0.1/test_flask'
+    MAIL_USERNAME = '*********@qq.com'
+    MAIL_RECEIVER = '*********@qq.com'
+    MAIL_PASSWORD = '*********'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:*********@127.0.0.1/test_flask'
 
 
 class TestingConfig(Config):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:31415926z@127.0.0.1/test_flask'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:*********@127.0.0.1/test_flask'
 
 
 class ProductionConfig(Config):
